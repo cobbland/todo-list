1f12d316eaea35a6267b62b2286154a3dfd7fe93
cobbland <jacobdensford@proton.me>
2025-02-20 14:06:59 -0500

    Add more styles and layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import "./styles.css";
+// Import external functions
 
+// Assign variables 
 const header = document.querySelector('header');
 const menu = document.querySelector('.menu');
 const content = document.querySelector('.content');
 const footer = document.querySelector('footer');
 
-header.textContent = "Header";
+// Add content to DOM via variables
 menu.textContent = "Menu";
 content.textContent = "Content";
-footer.textContent = "Footer";
+
+// 
